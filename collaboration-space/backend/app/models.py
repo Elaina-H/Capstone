7ad5b8e9391c24718f7efc93859a53c3b196c158
3DGamerexe <aaliyahmadcal@yahoo.com>
2025-03-08 00:09:34 -0600

    Reset backend folder

---
+++ collaboration-space/backend/app/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+# from azure.identity import DefaultAzureCredential
+# import pyodbc 
+
+# # Use DefaultAzureCredential to authenticate
+# credential = DefaultAzureCredential()
+
+# # Get the token for Azure SQL Database
+# token = credential.get_token("https://database.windows.net/.default").token
+
+# # Connection string for Azure SQL Database
+# conn_str = 'DRIVER={ODBC Driver 17 for SQL Server};' \
+#            'SERVER=educapstone-server.database.windows.net;' \
+#            'DATABASE=EduBoardDB;' \
+#            'Authentication=ActiveDirectoryMFA;'
+
+# # Establish the connection using the token
+# conn = pyodbc.connect(conn_str, attrs_before={1256: token})
+
+# # Query the database 
+# cursor = conn.cursor()
+# cursor.execute("Select @@Version")
+# row = cursor.fetchone()
+
+# print("Connected successfully")
+# ##print("SQL Server version: ", row[0])
