33420e03998a17e45e036a125df7c34f9005a815
3DGamerexe <aaliyahmadcal@yahoo.com>
2025-04-25 12:09:21 -0500

    Drop works
    bug - does not save when page refreshes

--- collaboration-space/frontend/src/Todo.js
+++ collaboration-space/frontend/src/Todo.js
@@ -13,6 +13,7 @@ const Todo = ({ tasks, addTask, deleteEventAndTask }) => {
   // };
 
   const [taskInput, setTaskInput] = useState("");
+  const [canvasItems, setCanvasItems] = useState([]); // holds the items inside the canvas
 
   const addNewTask = () => {
     if (taskInput.trim()) {
@@ -72,16 +73,33 @@ const Todo = ({ tasks, addTask, deleteEventAndTask }) => {
     };
   }, []);
 
-  // const [draggingIndex, setDraggingIndex] = useState(null);
-  // const [dragOffset, setDragOffset] = useState({ x: 0, y: 0 });
-
+  // Drag feature
   const handleDragStart = (e, task) => {
     e.dataTransfer.setData("text/plain", JSON.stringify(task));
   };
 
+  // Drop feature
   const handleDrop = (e) => {
+    e.preventDefault();
     console.log("dropped");
+
+    const data = e.dataTransfer.getData("text/plain");
+    const task = JSON.parse(data);
+    const canvasRect = e.target.getBoundingClientRect();
+
+    const x = e.clientX - canvasRect.left;
+    const y = e.clientY - canvasRect.top;
+
+    const droppedBox = {
+      ...task,
+      x,
+      y
+    };
+
+    setCanvasItems((prev) => [...prev, droppedBox]);
+
   };
+  
 
   return (
     <div className="outer">
@@ -99,7 +117,10 @@ const Todo = ({ tasks, addTask, deleteEventAndTask }) => {
             <li key={index}>
               <span
                 draggable
-                onDragStart={(e)=> handleDragStart(e, task)}
+                onDragStart={(e)=> {
+                  // handleDragStart(e, task)
+                  e.dataTransfer.setData("text/plain", JSON.stringify(task));
+                }}
               >
                 {task.text}
               </span> 
@@ -112,7 +133,32 @@ const Todo = ({ tasks, addTask, deleteEventAndTask }) => {
         <div id="canvas"
           onDragOver={(e) => e.preventDefault()}
           onDrop = {handleDrop}
-        ></div>
+          style={{ 
+            width: "3000px", 
+            height: "3000px", 
+            position: "relative", 
+            backgroundImage: "radial-gradiant(#333 1px, transparent 1px)",
+            backgroundSize: "30px 30 px"
+          }} 
+        >
+          {canvasItems.map((item, index) => (
+            <div
+              key={index}
+              // onMouseDown={(e) => mouseDown(e, index)}
+              style= {{
+                position: "absolute",
+                top: item.y,
+                left: item.x,
+                padding: "5px 10px",
+                backgroundColor: "#ddd",
+                borderRadius: "4px",
+                cursor: "grab"
+              }}
+            >
+              {item.text}
+            </div>
+          ))}
+        </div>
         {/* Additional content can go here */}
         
       </div>
